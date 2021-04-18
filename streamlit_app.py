bf520acd70722ff7b4fcf3a2309e44e43e153cf8
Muhammad Muzzammil <gpm1982@gmail.com>
2021-04-18 17:21:16 +0800

    Updated application file.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,35 +2,55 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in NYC')
+import gzip
+import io
+import urllib.request
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+st.title('MY Bursa Trading - Technical Analysis')
+
+#DATE_COLUMN = 'date/time'
+COUNTERS_URL = ('https://raw.githubusercontent.com/'
+            'gpm1982/my_bursa/main/csv/counters.csv.gzip')
+STOCKS_URL = ('https://raw.githubusercontent.com/'
+            'gpm1982/my_bursa/main/csv/stocks.csv.gzip')
 
 @st.cache
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
+def load_counters():
+    with urllib.request.urlopen(COUNTERS_URL) as url:
+        url_f = io.BytesIO(url.read())
+        gz = gzip.GzipFile(fileobj=url_f)
+
+    data = pd.read_csv(gz)
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    data = data.set_index(['symbol'])
     return data
 
-data_load_state = st.text('Loading data...')
-data = load_data(10000)
-data_load_state.text("Done! (using st.cache)")
+def load_stock_prices():
+    with urllib.request.urlopen(STOCKS_URL) as url:
+        url_f = io.BytesIO(url.read())
+        gz = gzip.GzipFile(fileobj=url_f)
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
+    data = pd.read_csv(gz)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data = data.set_index(['symbol'])
+    return data
 
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
+#data_load_state = st.text('Loading data...')
+data = load_counters()
+stock_prices = load_stock_prices()
+#data_load_state.text("Done!")
 
-# Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+#if st.checkbox('Show raw data'):
+#    st.subheader('Raw data')
+#    st.write(data)
 
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+stock_symbol = st.selectbox('Select Stock', data.index)
+stock_info = data.loc[stock_symbol]
+st.text("{0} ({1})".format(stock_info['corporatename'], stock_symbol))
+chart_df = stock_prices.loc[stock_symbol]
+chart_df = chart_df.set_index(pd.DatetimeIndex(chart_df['date'].values))
+chart_df = chart_df.drop(columns=['date'])
+#st.write(chart_df)
+st.line_chart(data=chart_df, width=0, height=0, use_container_width=True)
